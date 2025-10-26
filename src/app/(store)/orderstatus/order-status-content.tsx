1d6580e9f97721302d99feabfed4b4f6bbf0e2e3
Wesley Rafael <wesley.santos@opah.com>
2025-10-26 16:57:00 -0300

    fix: involving pages that search for search params in a suspense

---
+++ src/app/(store)/orderstatus/order-status-content.tsx
@@ -0,0 +1,41 @@
+"use client";
+
+import { OrderStatusList } from "@/components/molecules";
+import { creditCard, pix, ticket } from "./order-list";
+import { useSearchParams } from "next/navigation";
+
+type IPaymentTypeProps = "pix" | "ticket" | "creditCard";
+
+const OrderStatusContent = () => {
+  const searchParams = useSearchParams();
+
+  const paymentType = searchParams.get("payment_type");
+
+  const paymentMap = {
+    pix,
+    ticket,
+    creditCard,
+  } as const;
+
+  const orderList = paymentMap[paymentType as IPaymentTypeProps] ?? creditCard;
+
+  return (
+    <div className="pt-7">
+      <h1 className="text-lg font-bold text-primary text-center md:text-start">Status do pedido</h1>
+      <div className="pt-11 flex justify-center">
+        <div className="flex flex-col md:flex-row items-center gap-5 md:gap-0">
+          {orderList.map((item, index) => (
+            <OrderStatusList
+              key={item.title}
+              title={item.title}
+              icon={item.icon}
+              noUnderline={index !== orderList.length - 1}
+            />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default OrderStatusContent;
---
+++ src/app/(store)/payment/payment-content.tsx
@@ -0,0 +1,94 @@
+"use client";
+import { RenderCondition } from "@/components/atoms";
+import { Button } from "@/components/ui/button";
+import { Card } from "@/components/ui/card";
+import Image from "next/image";
+import { redirect, useSearchParams } from "next/navigation";
+import { IoIosTimer } from "react-icons/io";
+import { toast } from "react-toastify";
+
+const PaymentContent = () => {
+  const searchParams = useSearchParams();
+
+  const paymentType = searchParams.get("payment_type");
+
+  const redirectPage = (typePayment: string) => {
+    redirect(`/orderstatus?payment_type=${typePayment}`);
+  };
+
+  return (
+    <div className="pt-7 flex flex-col items-center gap-7">
+      <Card className="w-full md:w-2xl">
+        <div className="flex flex-col items-center text-center gap-2">
+          <p className="text-sm text-gray-600">Falta pouco!</p>
+          <p className="text-lg font-bold">Pague R$ 6.528,08 via Pix para concluir sua compra</p>
+        </div>
+      </Card>
+      <RenderCondition condition={paymentType === "ticket"}>
+        <Card className="w-full md:w-2xl">
+          <div className="px-4 text-center md:text-start">
+            <p className="text-lg font-bold">Instruções de pagamento</p>
+            <p className="text-gray-700 text-sm mt-2">
+              Para pagar pelo Internet Banking, copie a linha digitável ou escaneie o código de
+              barras. Para pagar em qualquer banco, caixa eletrônico ou lotérica, por favor, imprima
+              o boleto.
+            </p>
+            <div className="flex flex-col md:flex-row items-center gap-1 mt-4">
+              <IoIosTimer className="text-gray-600 text-lg" />
+              <p className="text-sm">
+                Aprovação em 1 ou 2 dias úteis. Os pagamentos feitos nos fins de semana ou feriados
+                serão aprovados no dia útil seguinte.
+              </p>
+            </div>
+
+            <div className="flex flex-col items-center justify-center py-5">
+              <Image src="/codigobarras.png" alt="codigo de barras" width={500} height={200} />
+              <p>42297.11504 00064.897317 08161.108629 1 12480000652299</p>
+              <Button onClick={() => toast.success("Copiado com sucesso!")} className="mt-3">
+                Copiar linha digitável
+              </Button>
+            </div>
+            <div className="flex justify-center pt-7" onClick={() => redirectPage("ticket")}>
+              <Button>Realizei o pagamento</Button>
+            </div>
+          </div>
+        </Card>
+      </RenderCondition>
+      <RenderCondition condition={paymentType === "pix"}>
+        <Card className="w-full md:w-2xl">
+          <div className="px-4 text-center md:text-start">
+            <p className="text-lg font-bold">Escaneie um código QR para pagar</p>
+            <div className="flex flex-col gap-1 px-1.5">
+              <p>1. Acesse seu Internet Banking ou app de pagamentos</p>
+              <p>2. Escolha pagar via Pix</p>
+              <p>3. Escaneie o código a seguir: </p>
+            </div>
+            <div className="flex justify-center py-5">
+              <Image src="/codigqr.png" alt="codigo qr" width={150} height={150} />
+            </div>
+            <div className="flex items-center justify-center md:justify-start gap-1">
+              <IoIosTimer className="text-gray-600" />
+              <p className="text-sm text-gray-600">Pague e será créditado na hora.</p>
+            </div>
+            <div className="border-t mt-4 pt-2">
+              <p className="text-lg font-bold">Ou copie este código para fazer o pagamento</p>
+              <div className="border rounded-sm mt-2 p-2.5">
+                <p className="text-gray-600 text-sm line-clamp-2">
+                  048451218.gov.bcb.pix.pixtestecodigo048451218.gov.bcb.pix.pixtestecodigo048451218.gov.bcb.pix.pixtestecodigo
+                </p>
+              </div>
+              <Button onClick={() => toast.success("Copiado com sucesso!")} className="mt-3">
+                Copiar código
+              </Button>
+            </div>
+            <div className="flex justify-center pt-7" onClick={() => redirectPage("pix")}>
+              <Button>Realizei o pagamento</Button>
+            </div>
+          </div>
+        </Card>
+      </RenderCondition>
+    </div>
+  );
+};
+
+export default PaymentContent;
