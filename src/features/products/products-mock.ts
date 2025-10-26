001b3af75852fecfe37c174affd4828ea9adf80b
Wesley Rafael <wesley.santos@opah.com>
2025-10-26 15:37:27 -0300

    feat: add new products

--- src/features/products/products-mock.ts
+++ src/features/products/products-mock.ts
@@ -1,13 +1,12 @@
 export const productsMock = [
   {
     id: 1,
-    title:
-      'Smartphone Xiaomi Redmi 14C BR 256GB 4GB RAM Virtual Tela 6" 120hz Câmera Traseira 50MP - Preto',
+    title: 'Notebook Lenovo V14 G4 AMN AMD Ryzen 3 7320U 8GB 256GB SSD DOS 14" - 83GE000CBR Preto',
     description:
-      'Smartphone Xiaomi Redmi 14C BR 256GB 4GB RAM Virtual Tela 6" 120hz Câmera Traseira 50MP - Preto',
-    price: 1299.91,
-    oldPrice: 1599.0,
-    imageUrl: "/products/cell.jpg",
+      'Notebook Lenovo V14 G4 AMN AMD Ryzen 3 7320U 8GB 256GB SSD DOS 14" - 83GE000CBR Preto',
+    price: 2159.1,
+    oldPrice: 2399.0,
+    imageUrl: "/products/notebook.jpeg",
     quantity: 1,
   },
   {
@@ -16,20 +15,38 @@ export const productsMock = [
       'Smartphone Xiaomi Redmi 14C BR 256GB 4GB RAM Virtual Tela 6" 120hz Câmera Traseira 50MP - Preto',
     description:
       'Smartphone Xiaomi Redmi 14C BR 256GB 4GB RAM Virtual Tela 6" 120hz Câmera Traseira 50MP - Preto',
-    price: 18299.0,
-    oldPrice: 21599.0,
+    price: 1299.0,
+    oldPrice: 1899.0,
     imageUrl: "/products/cell.jpg",
     quantity: 1,
   },
   {
     id: 3,
     title:
-      'Smartphone Xiaomi Redmi 14C BR 256GB 4GB RAM Virtual Tela 6" 120hz Câmera Traseira 50MP - Preto',
+      'Smartphone Motorola Moto G15 256GB 12GB RAM Boost Câmera 50MP com AI Tela 6.7" NFC - Grafite',
     description:
-      'Smartphone Xiaomi Redmi 14C BR 256GB 4GB RAM Virtual Tela 6" 120hz Câmera Traseira 50MP - Preto',
-    price: 3294.0,
+      'Smartphone Motorola Moto G15 256GB 12GB RAM Boost Câmera 50MP com AI Tela 6.7" NFC - Grafite',
+    price: 1399.0,
     oldPrice: 0,
-    imageUrl: "/products/cell.jpg",
+    imageUrl: "/products/motorola.jpeg",
+    quantity: 1,
+  },
+  {
+    id: 4,
+    title: "Micro-Ondas Mondial MO0121B 21 Litros Tira Odor Preto - 110v",
+    description: "Micro-Ondas Mondial MO0121B 21 Litros Tira Odor Preto - 110v",
+    price: 549.0,
+    oldPrice: 0,
+    imageUrl: "/products/microondas.webp",
+    quantity: 1,
+  },
+  {
+    id: 5,
+    title: "Tablet Samsung A9 EE 64GB 4G Tela 8.7 SM-X115NZAAL05",
+    description: "Tablet Samsung A9 EE 64GB 4G Tela 8.7 SM-X115NZAAL05",
+    price: 999.0,
+    oldPrice: 1099.9,
+    imageUrl: "/products/tablet.webp",
     quantity: 1,
   },
 ];
